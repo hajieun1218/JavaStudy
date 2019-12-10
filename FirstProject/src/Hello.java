9506a077d43bf32f778b1e5a112b80941a45866b
sist183 <sist183@DESKTOP-0NO7HG6>
2019-12-10 16:49:17 +0900

    -

---
+++ FirstProject/src/Hello.java
@@ -0,0 +1,33 @@
+/*
+ * 자바
+ * ===
+ * 데이터 저장  => 데이터 연산 처리(연산자) => 결과값 출력(순서, 제어문)   
+ * ========
+ *  메모리(한번 쓰고 버림 ex.변수) : 한개(변수), 여러개(배열, 클래스)
+ *  File(영구적)
+ *  RDBMS(영구적)
+ *  
+ *  ==> Spring / Mybatis (Framework) => java 를 제공
+ *      ================= 직접제작
+ *      
+ *      Framework => 레고
+ *      library => 완제품
+ *      
+ *      
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("안녕하세요!!");
+		
+		
+		  System.out.println("안녕하세요!!"); System.out.println("안녕하세요!!");
+		  System.out.println("안녕하세요!!"); System.out.println("안녕하세요!!");
+		 
+		 
+
+	}
+
+}
