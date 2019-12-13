271ccd5a8fb409c101dac48283011e37823e30f0
sist183 <sist183@DESKTOP-0NO7HG6>
2019-12-13 17:55:48 +0900

    -

--- 20191213-데이터처리(반복문-이중for)/src/이중반복문2.java
+++ 20191213-데이터처리(반복문-이중for)/src/이중반복문2.java
@@ -1,11 +1,31 @@
 /*
- *    
+ *    break : 반복을 중단할 때
+ *    continue : 특정 부분을 제외할 때 
  */
 public class 이중반복문2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for(int i=1; i<=10; i++) {
+			if(i==5)
+				break;
+			System.out.println(i);
+		}
+		
+		System.out.println("=== continue ===");
+		for(int i=1; i<=10; i++) {
+			if(i==5 || i==7)
+				continue;
+			System.out.println(i);
+		}
+		
+		System.out.println("=== 짝수만 출력 ===");
+		for(int i=1; i<=10; i++) {
+			if(i%2!=0)
+				continue;
+			System.out.println(i);
+		}
 	}
 
 }
---
+++ 20191213-데이터처리(반복문-이중for)/src/제어문정리.java
@@ -0,0 +1,23 @@
+/*
+ *    for(int i=1; i<=10; i++) {
+ *       System.out.println(i);
+ *    }
+ */
+public class 제어문정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		aaa();
+		aaa();
+		aaa();
+		
+		
+	}
+
+	public static void aaa() {
+		int a=10;
+		System.out.println("a="+a);
+		++a;
+	}
+}
