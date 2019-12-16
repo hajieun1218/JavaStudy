50dff425c7dd8ecb4c0088f81b424df88ef62e84
sist183 <sist183@DESKTOP-0NO7HG6>
2019-12-16 16:54:01 +0900

    -

--- 20191216-데이터관리(배열)/src/배열11.java
+++ 20191216-데이터관리(배열)/src/배열11.java
@@ -2,6 +2,8 @@
  *   5개의 난수 발생 ==> 배열
  *   30 20 40 50 10
  *   ===============> 50 40 30 20 10
+ *   
+ *   <<선택정렬>>
  */
 public class 배열11 {
 
@@ -9,17 +11,16 @@ public class 배열11 {
 		// TODO Auto-generated method stub
 
 		int[] num = new int[5];
-		int temp=0;
 		
 		for(int i=0; i<5; i++) {
 			num[i] = (int)(Math.random()*100) + 1;
 			System.out.print(num[i] + " ");
 		}
 		
-		for(int i=0; i<5; i++) {
-			for(int j=i; j<5; j++) {
+		for(int i=0; i<num.length-1; i++) {
+			for(int j=i+1; j<num.length; j++) {
 				if(num[i]<num[j]) {
-					temp = num[i];
+					int temp = num[i];
 					num[i] = num[j];
 					num[j] = temp;
 				}
